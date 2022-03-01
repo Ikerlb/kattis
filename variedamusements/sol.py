4d6121a71dab832badb9731fa5c7cc488d225cad
Iker Lissarrague <ikerlissarrague@192.168.68.106>
2022-03-01 01:04:16 -0600

    slight change to variedamusements

--- variedamusements/sol.py
+++ variedamusements/sol.py
@@ -1,22 +1,43 @@
-def A(n, a, b, c):
-    if n == 0:
-        return 1
-    return b * B(n - 1, a, b, c) + c * C(n - 1, a, b, c)
+from functools import lru_cache
 
-def B(n, a, b, c):
-    if n == 0:
-        return 1
-    return a * A(n - 1, a, b, c) + c * C(n - 1, a, b, c)
+#lru_cache(None)
+#def A(n):
+#    if n == 0:
+#        return 1
+#    cb = (b * B(n - 1)) % MOD
+#    cc = (c * C(n - 1)) % MOD
+#    return (cb + cc) % MOD
 
-def C(n, a, b, c):
-    if n == 0:
-        return 1
-    return a * A(n - 1, a, b, c) + b * B(n - 1, a, b, c)
+#lru_cache(None)
+#def B(n):
+#    if n == 0:
+#        return 1
+#    ca = (a * A(n - 1)) % MOD
+#    cc = (c * C(n - 1)) % MOD
+#    return (ca + cc) % MOD
 
+#lru_cache(None)
+#def C(n):
+#    if n == 0:
+#        return 1
+#    ca = (a * A(n - 1)) % MOD
+#    cb = (b * B(n - 1)) % MOD
+#    return (ca + cb) % MOD
+
+MOD = 10 ** 9 + 7
 n, a, b, c = map(int, input().split(" "))
 
-ca = a * A(n - 1, a, b, c)
-cb = b * B(n - 1, a, b, c)
-cc = c * C(n - 1, a, b, c)
+A = [1] * n
+B = [1] * n
+C = [1] * n
 
-print((ca + cb + cc) % (10 ** 9 + 7))
+for i in range(1, n):
+    A[i] = (((b * B[i - 1]) % MOD) + ((c * C[i - 1]) % MOD)) % MOD
+    B[i] = (((a * A[i - 1]) % MOD) + ((c * C[i - 1]) % MOD)) % MOD
+    C[i] = (((b * B[i - 1]) % MOD) + ((a * A[i - 1]) % MOD)) % MOD
+
+ca = (a * A[n - 1]) % MOD
+cb = (b * B[n - 1]) % MOD
+cc = (c * C[n - 1]) % MOD
+
+print((ca + cb + cc) % MOD)
