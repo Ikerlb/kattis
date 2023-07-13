486d4c67eaec7a8408fa8dbddcc140f2276bfc57
Iker Lissarrague <iker.lissarrague@vivaaerobus.com>
2023-07-12 19:02:56 -0600

    good old rabin karp

--- geneticsearch/sol.py
+++ geneticsearch/sol.py
@@ -1,33 +1,62 @@
-def z_array(p):
-    prefix = [0 for _ in range(len(p))]
-    j = None
-    while i < len(p):
-        while j is not None and p[i] != p[j]:
-            j = p[j]
-        j += 1
-        i += 1
-        p[i] = j
-    return prefix
+from collections import Counter
 
-def kmp(p, s, z):
-    i = j = 0
-    res = 0
-    while i < len(p):
-        while j >= 0 and p[j] != s[i]:
-            j = z[j]
-        i += 1
-        j += 1
-        if j == len(p):
-            res += 1
+def _hash(s, d, q):
+    return sum((d[c] * ((len(d)+1) ** i)) % q for i, c in enumerate(reversed(s))) % q
+
+def rabin_karp_hashes(s, ws, q, d):
+    base = len(d) + 1
+
+    c = Counter()
+    
+    sh = _hash(s[:ws], d, q)
+    c[sh] += 1
+
+    for i in range(ws, len(s)):
+        li = i - ws
+
+        sh -= d[s[li]] * (base ** (ws - 1))
+        sh *= base
+        sh += d[s[i]]
+        sh = sh % q
+
+        c[sh] += 1
+
+    return c
+
+def remove_one(s):
+    res = set()
+    for i in range(len(s)):
+        res.add(s[:i] + s[i + 1:])
     return res
+
+def add_one(s, d):
+    res = set()
+    for i in range(len(s) + 1):
+        for c in d.keys(): 
+            res.add(s[:i] + c + s[i:])
+    return res
+
+d = {
+    "A": 1,
+    "G": 2,
+    "C": 3,
+    "T": 4,
+}
+
+q = 10 ** 9 + 7
 
 while True:
     l = input()
     if l == "0":
         break
-    p, s = input().split(" ")
-    z = z_array(p)
-    print(kmp(p, s, z))
-    
 
+    p, s = l.split(" ")
+    c = rabin_karp_hashes(s, len(p), q, d)
+    cmo = rabin_karp_hashes(s, len(p) - 1, q, d)
+    cpo = rabin_karp_hashes(s, len(p) + 1, q, d)
 
+    t1 = c[_hash(p, d, q)]
+    t2 = sum(cmo[_hash(pmo, d, q)] for pmo in remove_one(p))
+    t3 = sum(cpo[_hash(ppo, d, q)] for ppo in add_one(p, d))
+
+    print(f"{t1} {t2} {t3}")
