8f5a0c5bbcca4ae39861596acecfca7deb567c45
Iker Lissarrague <iker.lissarrague@vivaaerobus.com>
2023-07-15 01:35:04 -0600

    this one wasn't too hard

---
+++ checkingforcorrectness/sol.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+def _pow(a, k, mod):
+    if k == 0:
+        return 1
+    if k % 2 == 0:
+        h = _pow(a, k >> 1, mod)
+        return (h * h) % mod
+    h = _pow(a, k >> 1, mod)
+    return (a * h * h) % mod
+
+def _eval(a, op, b):
+    if op == "+":
+        return a + b
+    if op == "*":
+        return a * b
+    if op == "^":
+        return pow(a, b, 10000)
+
+for line in stdin:
+    a, op, b = line[:-1].split(" ")
+    s = str(_eval(int(a), op, int(b)))
+    print(int(s[-4:]))
