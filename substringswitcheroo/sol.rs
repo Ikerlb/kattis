6685466310586bbfff188511871daaf23418dbce
Iker Lissarrague <iker.lissarrague@vivaaerobus.com>
2023-07-21 00:19:55 -0600

    fixed some bugs and added some comments to typo

--- substringswitcheroo/sol.rs
+++ substringswitcheroo/sol.rs
@@ -9,7 +9,7 @@ fn count(a: &[usize]) -> [usize; 26] {
     return res;
 }
 
-fn pow(a: usize, b: usize, p: usize) -> usize {
+/*fn pow(a: usize, b: usize, p: usize) -> usize {
     if b == 0 {
         return 1;
     }
@@ -18,6 +18,18 @@ fn pow(a: usize, b: usize, p: usize) -> usize {
         return (((half * half) % p) * a) % p;
     } else {
         return (half * half) % p;
+    }
+}*/
+
+fn pow(a: usize, b: usize, p: usize) -> usize {
+    if b == 0 {
+        return 1;
+    }
+    let half = pow(a, b >> 1, p);
+    if b & 1 == 1 {
+        return mod_mult(mod_mult(half, half, p), a, p);
+    } else {
+        return mod_mult(half, half, p);
     }
 }
 
@@ -30,7 +42,14 @@ fn hash(a: &[usize], bases: &Vec<usize>, p: usize) -> usize {
         .iter()
         .enumerate()
         .map(|(i, &f)| pow(bases[i], f, p))
-        .fold(1, |acc, e| (acc * e) % p);
+        .fold(1, |acc, e| mod_mult(acc, e, p));
+}
+
+fn to_string(a: &[usize]) -> String {
+    return a
+        .iter()
+        .map(|&x| char::from_u32((x + ('a' as usize)) as u32).unwrap())
+        .collect::<String>();
 }
 
 /* 
@@ -45,6 +64,10 @@ fn merge(hashes: &Vec<usize>) -> String {
         .join(",");
 }
 
+fn mod_mult(a: usize, b: usize, m: usize) -> usize {
+    return ((a as u128 * b as u128) % (m as u128)) as usize;
+}
+
 fn has_anagramic_substrings(a: &Vec<usize>, b: &Vec<usize>, bases: &Vec<usize>, inverses: &Vec<Vec<usize>>, ps: &Vec<usize>, k: usize) -> Option<usize> {
     let mut cbs = HashSet::new();
 
@@ -55,8 +78,8 @@ fn has_anagramic_substrings(a: &Vec<usize>, b: &Vec<usize>, bases: &Vec<usize>, 
     cbs.insert(merge(&hbs)); 
     for i in k..b.len() {
         for j in 0..ps.len() {
-            hbs[j] = (hbs[j] * inverses[j][b[i - k]]) % ps[j];
-            hbs[j] = (hbs[j] * bases[b[i]]) % ps[j];
+            hbs[j] = mod_mult(hbs[j], inverses[j][b[i - k]], ps[j]);
+            hbs[j] = mod_mult(hbs[j], bases[b[i]], ps[j]);
         }
         cbs.insert(merge(&hbs));
     }
@@ -66,16 +89,14 @@ fn has_anagramic_substrings(a: &Vec<usize>, b: &Vec<usize>, bases: &Vec<usize>, 
         .map(|&p| hash(&a[..k], bases, p))
         .collect::<Vec<_>>();
         
-    //println!("a -> {}: hash is {}", to_string(&a[..k]), merge(&has));
     if cbs.contains(&merge(&has)) {
         return Some(0);
     }
     for i in k..a.len() {
         for j in 0..ps.len() {
-            has[j] = (has[j] * inverses[j][a[i - k]]) % ps[j];
-            has[j] = (has[j] * bases[a[i]]) % ps[j];
+            has[j] = mod_mult(has[j], inverses[j][a[i - k]], ps[j]);
+            has[j] = mod_mult(has[j], bases[a[i]], ps[j]);
         }
-        //println!("a -> {}: hash is {}", to_string(&a[i-k +1..=i]), merge(&has));
         if cbs.contains(&merge(&has)) {
             return Some(i - k + 1);
         }
@@ -96,6 +117,12 @@ fn solve(s1: &Vec<usize>, s2: &Vec<usize>, bases: &Vec<usize>, inverses: &Vec<Ve
     return "NONE".to_string();
 }
 
+/*
+ * Fermat's little theorem:
+ * a^(p - 1) % p == 1
+ * which means that since a*(a^(p - 2)) == 1 
+ * a^(p-2) is a modulo inverse of a
+ */
 fn create_inverses(bases: &Vec<usize>, modulo: usize) -> Vec<usize> {
     return bases
         .iter()
@@ -110,13 +137,20 @@ fn main() -> io::Result<()> {
     let tcs = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
 
 
-    let modulos = vec![1000000007, 1000000009];
+    // my program definitely had overflowing bugs
+    // would now need a little more consistency on 
+    // the concrete types (usize could be u32 or u64)
+    // this would work ok (that would be u64)
+    // as anything over u128 would simply overflow on some 
+    // multiplications, even if the modulo keeps it within 
+    // bounds, TODO: try with experimental widening_mult!
+    // kinda miss python amirite?
+    let modulos = vec![614244627451450103]; 
     let bases = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101];
     let inverses = &modulos
         .iter()
         .map(|&modulo| create_inverses(&bases, modulo))
         .collect::<Vec<_>>();
-
     for _ in 0..tcs {
         // ugh much better to just collect the lines
         let s1 = lines
